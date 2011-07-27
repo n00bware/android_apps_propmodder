49d4b6ebc1b54e68b5876a5326905eee2af497d6
Jon Stanford <jbirdvegas@gmail.com>
2011-07-27 01:53:09 -0400

    clean up Android.mk

--- Android.mk
+++ Android.mk
@@ -5,9 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := n00bware
+LOCAL_PACKAGE_NAME := propmodder
 
 include $(BUILD_PACKAGE)
 
-# Use the folloing include to make our test apk.
-include $(call all-makefiles-under,$(LOCAL_PATH))
