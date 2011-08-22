d24db585bb009982fe60a2e888024fb4e1665e24
JBirdVegas <jbirdvegas@gmail.com>
2011-08-22 13:29:21 -0400

    add some resources needed for market

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := propmodder
+LOCAL_PACKAGE_NAME := PropModder
 
 include $(BUILD_PACKAGE)
 
