a5051c3d1dd5c66f490007e88356ce670e84552a
JBirdVegas <jbirdvegas@gmail.com>
2011-10-05 16:39:07 -0400

    add credit screen and bump to v1.5.2

---
+++ src/com/n00bware/propmodder/Credit.java
@@ -0,0 +1,63 @@
+
+package com.n00bware.propmodder;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.content.Intent;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceScreen;
+
+
+public class Credit extends PreferenceActivity implements
+        Preference.OnPreferenceChangeListener {
+
+    private static final String JBIRDVEGAS_PREF = "pref_jbirdvegas";
+    private static final String REVNUMBERS_PREF = "pref_revnumbers";
+    private static final String SPITEMARE_PREF = "pref_spitemare";
+
+    private ListPreference mJBirdVegas;
+    private ListPreference mRevNumbers;
+    private ListPreference mSpitemare;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+        setTitle(R.string.credit_title);
+        addPreferencesFromResource(R.xml.credit);
+        PreferenceScreen prefSet = getPreferenceScreen();
+
+        mJBirdVegas = (ListPreference) prefSet.findPreference(JBIRDVEGAS_PREF);
+        mJBirdVegas.setOnPreferenceChangeListener(this);
+
+        mRevNumbers = (ListPreference) prefSet.findPreference(REVNUMBERS_PREF);
+        mRevNumbers.setOnPreferenceChangeListener(this);
+
+        mSpitemare = (ListPreference) prefSet.findPreference(SPITEMARE_PREF);
+        mSpitemare.setOnPreferenceChangeListener(this);
+
+    }
+
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        if (newValue != null) {
+            if (preference != null) {
+                return launchBrowser(newValue.toString());
+            }
+        }
+
+        return false;
+    }
+
+    private boolean launchBrowser(String urlValue) {
+        if (urlValue != null) {
+            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(urlValue));
+            startActivity(browserIntent);
+			return true;
+        }
+
+        return false;
+    }
+}
